1fc97c986887ca854c8dbc3a7b1ccaff065ebe30
jemima2004 <jemima.cse.20230104133@aust.edu>
2026-04-11 02:47:47 +0600

    save all changes

--- Backend/database/migrations/2026_03_06_184550_create_assessments_table.php
+++ Backend/database/migrations/2026_03_06_184550_create_assessments_table.php
@@ -6,27 +6,24 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('assessments', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('student_id');
             $table->unsignedBigInteger('course_id');
+            $table->string('title');
             $table->string('type');
-            $table->integer('marks');
+            $table->integer('total_marks');
+            $table->date('date');
             $table->timestamps();
-        $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
-        $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
-        
+
+            $table->foreign('course_id')
+                ->references('id')
+                ->on('courses')
+                ->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('assessments');

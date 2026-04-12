49e4cf0b30dc4c56fed88ad27683245cd96b18a9
Dola138 <dola.cse.20230104138@aust.edu>
2026-04-12 10:33:02 +0600

    api.php

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -8,60 +8,14 @@ use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\AssessmentController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ComplainController;
-<<<<<<< HEAD
 use App\Http\Controllers\AdminController;
 
-<<<<<<< Updated upstream
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-=======
-use App\Http\Controllers\AdminController; 
-
->>>>>>> 129083f9fea8fe2659475650d44a9c2b2c1cb1ef
-
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-<<<<<<< HEAD
-
-// Teacher Login Route
-=======
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
->>>>>>> Stashed changes
-=======
->>>>>>> 129083f9fea8fe2659475650d44a9c2b2c1cb1ef
 Route::post('/teacher/login', [TeacherController::class, 'login']);
 Route::get('/admin-stats', [AdminController::class, 'getStats']);
 Route::apiResource('assessments', AssessmentController::class);
 
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-// API Resources for CRUD operations
-Route::apiResource('students', StudentController::class);
-Route::apiResource('teachers', TeacherController::class);
-Route::apiResource('courses', CourseController::class);
-Route::apiResource('enrollments', EnrollmentController::class);
-Route::apiResource('attendances', AttendanceController::class);
-Route::apiResource('assessments', AssessmentController::class);
-
-/*
-// Dummy CRUD operations for items using UsersController
-Route::get('/items', [UsersController::class, 'index']);
-Route::get('/items/{id}', [UsersController::class, 'show']);
-Route::post('/items', [UsersController::class, 'store']);
-Route::put('/items/{id}', [UsersController::class, 'update']);
-Route::patch('/items/{id}', [UsersController::class, 'patch']);
-Route::delete('/items/{id}', [UsersController::class, 'destroy']);
-*/
-=======
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [AuthController::class, 'user']);
     Route::post('/logout', [AuthController::class, 'logout']);
@@ -70,34 +24,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/add-complain', [ComplainController::class, 'store']);
     Route::get('/advisor/complains', [ComplainController::class, 'getAdvisorComplains']);
     Route::get('/student/complains/{id}', [ComplainController::class, 'getStudentComplains']);
-=======
-Route::get('/admin-stats', [AdminController::class, 'getStats']); 
-
-Route::apiResource('assessments', AssessmentController::class);
-
-Route::middleware('auth:sanctum')->group(function () {
-
-    Route::get('/user', [AuthController::class, 'user']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::post('/logout-all', [AuthController::class, 'logoutAll']);
-
-    Route::get('/course-students/{course_id}', [AttendanceController::class, 'getStudentsByCourse']);
-
-    Route::post('/add-complain', [ComplainController::class, 'store']);
-
-    Route::get('/advisor/complains', [ComplainController::class, 'getAdvisorComplains']);
-    Route::get('/student/complains/{id}', [ComplainController::class, 'getStudentComplains']);
-
->>>>>>> 129083f9fea8fe2659475650d44a9c2b2c1cb1ef
     Route::apiResource('students', StudentController::class);
     Route::apiResource('teachers', TeacherController::class);
     Route::apiResource('enrollments', EnrollmentController::class);
     Route::apiResource('attendances', AttendanceController::class);
     Route::apiResource('courses', CourseController::class);
-<<<<<<< HEAD
 });
->>>>>>> Stashed changes
-=======
-
-});
->>>>>>> 129083f9fea8fe2659475650d44a9c2b2c1cb1ef
